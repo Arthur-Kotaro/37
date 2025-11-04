975f111fe9841649222f5f2bccdaac1ecf9b043d
Arthur-Kotaro <arthur.kotaro@yandex.ru>
2025-11-04 13:29:27 +0000

    prepared

--- 1/calc_backend.h
+++ 1/calc_backend.h
@@ -1,22 +1,25 @@
-//
 // Created by Kotaro on 04.11.2025.
-//
 
 #ifndef INC_1_CALC_BACKEND_H
 #define INC_1_CALC_BACKEND_H
-
+#include <chrono>
+#include <string>
 #include <QMainWindow>
 #include <QtWidgets/QLineEdit>
 
 class Calculator : public QMainWindow
 {
-    bool first_number = true;
+    bool first_number;
+    bool first_num_float, second_num_float;
+    long int first_num_i, second_num_i;
+    double first_num_f, second_num_f;
+    char operator_symbol;
     Q_OBJECT
 public:
     QLineEdit* LineEdit_value1 = nullptr;
     QLineEdit* LineEdit_value2 = nullptr;
     QLineEdit* LineEdit_result = nullptr;
-    explicit Calculator(QWidget* parent = nullptr): QMainWindow(parent) {}
+    explicit Calculator(QWidget* parent = nullptr): QMainWindow(parent), first_number(true), first_num_float(false), second_num_float(false){}
 public slots:
     void add0()
     {
@@ -70,13 +73,27 @@ public slots:
     };
     void decimal_point()
     {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "0");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "0");
+        if(first_number && !first_num_float) 
+        {
+            LineEdit_value1->setText(LineEdit_value1->text() + ",");
+            first_num_float = true;
+        }
+        else if(!first_number && !second_num_float)
+        {
+            LineEdit_value2->setText(LineEdit_value2->text() + ",");
+            second_num_float = true;
+        }
     };
     void addition()
     {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "0");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "0");
+        if(first_number)
+        {
+            first_number = false;
+            operator_symbol = '+';
+            if(first_num_float) first_num_i = std::strtod();
+            else first_num_float = std::strtol();
+            LineEdit_value1->setText(LineEdit_value1->text() + " " + operator_symbol);
+        }
     };
     void substraction()
     {
@@ -95,15 +112,24 @@ public slots:
     void reset()
     {
         first_number = true;
-        LineEdit_value1->text().clear();
-        LineEdit_value2->text().clear();
-        LineEdit_result->text().clear();
+        first_num_float = second_num_float = false;
+        LineEdit_value1->text().erase();
+        LineEdit_value2->text().erase();
+        LineEdit_result->text().erase();
         update();
 
     };
     void calculate()
     {
-
+        if(!first_number)
+        {
+            if (second_num_float) second_num_i = std::strtod();
+            else second_num_float = std::strtol();
+            if (first_num_float && second_num_float) LineEdit_result->setText(first_num_f + second_num_f);
+            if (!first_num_float && !second_num_float) LineEdit_result->setText(first_num_i + second_num_i);
+            if (first_num_float && !second_num_float) LineEdit_result-> setText(first_num_f + (double)second_num_i);
+            if (!first_num_float && second_num_float) LineEdit_result->setText((double)first_num_i + second_num_f);
+        }
     };
 };
 
