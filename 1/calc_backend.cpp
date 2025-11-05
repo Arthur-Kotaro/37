d26b969dd39a9f1921b386198867782c1488838b
arthur-kotaro <arthur.kotaro@yandex.ru>
2025-11-05 13:37:51 +0400

    task1 -addition

---
+++ 1/calc_backend.cpp
@@ -0,0 +1,121 @@
+#include "calc_backend.h"
+
+void Calculator::add0()
+{
+    if(cursor_on_first_number) LineEdit_value1->setText(LineEdit_value1->text() + "0");
+    else  LineEdit_value2->setText(LineEdit_value2->text() + "0");
+};
+void Calculator::add1()
+{
+    if(cursor_on_first_number) LineEdit_value1->setText(LineEdit_value1->text() + "1");
+    else  LineEdit_value2->setText(LineEdit_value2->text() + "1");
+};
+void Calculator::add2()
+{
+    if(cursor_on_first_number) LineEdit_value1->setText(LineEdit_value1->text() + "2");
+    else  LineEdit_value2->setText(LineEdit_value2->text() + "2");
+};
+void Calculator::add3()
+{
+    if(cursor_on_first_number) LineEdit_value1->setText(LineEdit_value1->text() + "3");
+    else  LineEdit_value2->setText(LineEdit_value2->text() + "3");
+};
+void Calculator::add4()
+{
+    if(cursor_on_first_number) LineEdit_value1->setText(LineEdit_value1->text() + "4");
+    else  LineEdit_value2->setText(LineEdit_value2->text() + "4");
+};
+void Calculator::add5()
+{
+    if(cursor_on_first_number) LineEdit_value1->setText(LineEdit_value1->text() + "5");
+    else  LineEdit_value2->setText(LineEdit_value2->text() + "5");
+};
+void Calculator::add6()
+{
+    if(cursor_on_first_number) LineEdit_value1->setText(LineEdit_value1->text() + "6");
+    else  LineEdit_value2->setText(LineEdit_value2->text() + "6");
+};
+void Calculator::add7()
+{
+    if(cursor_on_first_number) LineEdit_value1->setText(LineEdit_value1->text() + "7");
+    else  LineEdit_value2->setText(LineEdit_value2->text() + "7");
+};
+void Calculator::add8()
+{
+    if(cursor_on_first_number) LineEdit_value1->setText(LineEdit_value1->text() + "8");
+    else  LineEdit_value2->setText(LineEdit_value2->text() + "8");
+};
+void Calculator::add9()
+{
+    if(cursor_on_first_number) LineEdit_value1->setText(LineEdit_value1->text() + "9");
+    else  LineEdit_value2->setText(LineEdit_value2->text() + "9");
+};
+
+Calculator::Calculator(QWidget* parent): QMainWindow(parent), cursor_on_first_number(true), first_num_double(false), second_num_double(false){}
+
+void Calculator::addition() { calc_operator('+'); };
+void Calculator::substraction() { calc_operator('-'); };
+void Calculator::multiplication() { calc_operator('*'); };
+void Calculator::division() { calc_operator('/'); };
+
+
+void Calculator::decimal_point()
+{
+    if(cursor_on_first_number && !first_num_double)
+    {
+        LineEdit_value1->setText(LineEdit_value1->text() + ".");
+        first_num_double = true;
+    }
+    else if(!cursor_on_first_number && !second_num_double)
+    {
+        LineEdit_value2->setText(LineEdit_value2->text() + ".");
+        second_num_double = true;
+    }
+};
+
+void Calculator::reset()
+{
+    cursor_on_first_number = true;
+    first_num_double = false;
+    second_num_double = false;
+    LineEdit_value1->clear();
+    LineEdit_value2->clear();
+    LineEdit_result->clear();
+};
+
+void Calculator::calc_operator(char in_symbol)
+{
+    if(cursor_on_first_number)
+    {
+        cursor_on_first_number = false;
+        operator_symbol = in_symbol;
+        if(first_num_double) first_num_d = LineEdit_value1->text().toDouble();
+        else first_num_i = LineEdit_value1->text().toLong();
+        LineEdit_value1->setText(LineEdit_value1->text() + " " + operator_symbol);
+    }
+}
+
+void Calculator::calculate()
+{
+    if(!cursor_on_first_number)
+    {
+        if (second_num_double) second_num_d = LineEdit_value2->text().toDouble();
+        else second_num_i = LineEdit_value2->text().toLong();
+        if (!first_num_double && !second_num_double)
+        {
+            result_i = first_num_i + second_num_i;
+            LineEdit_result->setText(QString::number(result_i));
+        }
+        else
+        {
+            if (first_num_double && second_num_double)      result_d = first_num_d + second_num_d;
+            if (first_num_double && !second_num_double)     result_d = first_num_d + static_cast<double>(second_num_i);
+            if (!first_num_double && second_num_double)     result_d = static_cast<double>(first_num_i) + second_num_d;
+            LineEdit_result->setText(QString::number(result_d));
+        }
+#ifdef DEBUG
+        std::cout << "first_num_i = "<< first_num_i << std::endl << "second_num_i = "<< second_num_i << std::endl << "result_i = " << result_i << std::endl << std::endl
+        << "first_num_d = "<< first_num_d << std::endl << "second_num_d = "<< second_num_d << std::endl << "result_d = " << result_d << std::endl << std::endl;
+#endif
+    }
+};
--- 1/calc_backend.h
+++ 1/calc_backend.h
@@ -1,136 +1,55 @@
-// Created by Kotaro on 04.11.2025.
+#define DEBUG
+#ifdef DEBUG
+#include <iostream>
+#endif
 
 #ifndef INC_1_CALC_BACKEND_H
 #define INC_1_CALC_BACKEND_H
 #include <chrono>
 #include <string>
+#include <cstdlib>
 #include <QMainWindow>
 #include <QtWidgets/QLineEdit>
 
 class Calculator : public QMainWindow
 {
-    bool first_number;
-    bool first_num_float, second_num_float;
-    long int first_num_i, second_num_i;
-    double first_num_f, second_num_f;
+    
+private:
+    bool cursor_on_first_number, first_num_double, second_num_double;
+    long int first_num_i, second_num_i, result_i;
+    double first_num_d, second_num_d, result_d;
     char operator_symbol;
+
+    void calc_operator(char in_symbol);
+
     Q_OBJECT
+
 public:
     QLineEdit* LineEdit_value1 = nullptr;
     QLineEdit* LineEdit_value2 = nullptr;
     QLineEdit* LineEdit_result = nullptr;
-    explicit Calculator(QWidget* parent = nullptr): QMainWindow(parent), first_number(true), first_num_float(false), second_num_float(false){}
+
+    explicit Calculator(QWidget* parent = nullptr);
+
 public slots:
-    void add0()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "0");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "0");
-    };
-    void add1()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "1");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "1");
-    };
-    void add2()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "2");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "2");
-    };
-    void add3()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "3");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "3");
-    };
-    void add4()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "4");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "4");
-    };
-    void add5()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "5");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "5");
-    };
-    void add6()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "6");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "6");
-    };
-    void add7()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "7");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "7");
-    };
-    void add8()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "8");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "8");
-    };
-    void add9()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "9");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "9");
-    };
-    void decimal_point()
-    {
-        if(first_number && !first_num_float) 
-        {
-            LineEdit_value1->setText(LineEdit_value1->text() + ",");
-            first_num_float = true;
-        }
-        else if(!first_number && !second_num_float)
-        {
-            LineEdit_value2->setText(LineEdit_value2->text() + ",");
-            second_num_float = true;
-        }
-    };
-    void addition()
-    {
-        if(first_number)
-        {
-            first_number = false;
-            operator_symbol = '+';
-            if(first_num_float) first_num_i = std::strtod();
-            else first_num_float = std::strtol();
-            LineEdit_value1->setText(LineEdit_value1->text() + " " + operator_symbol);
-        }
-    };
-    void substraction()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "0");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "0");
-    };
-    void multiplication()
-    {
-        if(first_number) LineEdit_value1->setText(LineEdit_value1->text() + "0");
-        else  LineEdit_value2->setText(LineEdit_value1->text() + "0");
-    };
-    void division()
-    {
-
-    };
-    void reset()
-    {
-        first_number = true;
-        first_num_float = second_num_float = false;
-        LineEdit_value1->text().erase();
-        LineEdit_value2->text().erase();
-        LineEdit_result->text().erase();
-        update();
-
-    };
-    void calculate()
-    {
-        if(!first_number)
-        {
-            if (second_num_float) second_num_i = std::strtod();
-            else second_num_float = std::strtol();
-            if (first_num_float && second_num_float) LineEdit_result->setText(first_num_f + second_num_f);
-            if (!first_num_float && !second_num_float) LineEdit_result->setText(first_num_i + second_num_i);
-            if (first_num_float && !second_num_float) LineEdit_result-> setText(first_num_f + (double)second_num_i);
-            if (!first_num_float && second_num_float) LineEdit_result->setText((double)first_num_i + second_num_f);
-        }
-    };
+    void add0();
+    void add1();
+    void add2();
+    void add3();
+    void add4();
+    void add5();
+    void add6();
+    void add7();
+    void add8();
+    void add9();
+   
+    void decimal_point();
+    void addition();
+    void substraction();
+    void multiplication();
+    void division();
+    void calculate();
+    void reset();
 };
 
 #endif //INC_1_CALC_BACKEND_H
